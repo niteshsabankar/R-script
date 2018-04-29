4f2f4ff36442fbedb1aa936ed597473b01a7e2df
Nitesh sabankar <niteshsabankar@gmail.com>
2018-04-29 03:50:51 -0700

    Create Gene ID list according to modules

---
+++ CreateGeneIDList.R
@@ -0,0 +1,12 @@
+# Variable Clist contains 10 highly preserved modules
+# Requires A1 and A2 directories already presernt in 'present working directory'
+
+clist <- c("blue", "darkred", "saddlebrown", "darkorange", "red", "brown", "violet", "royalblue", "black", "green")
+for (i in clist) {
+	
+   i <- noquote(i)
+   A1path <- paste("A1/", paste(i, "A1", sep=""), sep="")
+   A2path <- paste("A2/", paste(i, "A2", sep=""), sep="")
+   write.table(rownames(modulesA1[grep(paste("^", i, "$", sep = ""),modulesA1$module),]), file=A1path, col.names=FALSE, row.names=FALSE, quote=FALSE)
+   write.table(rownames(modulesA2[grep(paste("^", i, "$", sep = ""),modulesA2$module),]), file=A2path, col.names=FALSE, row.names=FALSE, quote=FALSE)
+}
