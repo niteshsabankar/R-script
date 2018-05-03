dc15cdc7b0ba84fc82b4768519296348c8f2549c
Nitesh sabankar <niteshsabankar@gmail.com>
2018-05-03 03:44:47 -0700

    Check Rank Expression code

--- 2.rankExpression.R
+++ 2.rankExpression.R
@@ -7,10 +7,6 @@ softPower = 18
 rankExprA1= rank(rowMeans(datExprA1))
 rankExprA2= rank(rowMeans(datExprA2))
 
-random= sample(commonProbes)
-rankConnA1= rank(softConnectivity(t(datExprA1[random,]),type="signed",power=softPower))
-rankConnA2= rank(softConnectivity(t(datExprA2[random,]),type="signed",power=softPower))
-
 pdf("generalNetworkProperties.pdf", height=10, width=12)
 par(mar=c(5,10,4,10)+.1)
 verboseScatterplot(rankExprA1,rankExprA2, xlab="Ranked Expression (G. Oceanica)", ylab="Ranked Expression (E. huxleyi)", pch = 1)
