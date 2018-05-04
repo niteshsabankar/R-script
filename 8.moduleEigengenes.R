0989b48e911c9d4a8b36f28964e85d13affa4f32
Nitesh sabankar <niteshsabankar@gmail.com>
2018-05-04 05:48:23 -0700

    comments

--- 8.moduleEigengenes.R
+++ 8.moduleEigengenes.R
@@ -5,5 +5,5 @@ PCs2A = moduleEigengenes(t(datExprA2g), colors=moduleColors)
 ME_2A = PCs2A$eigengenes
 
 # Keep module Eigengenes of only 10 highly preserved modules
-ME_1A_preserved <- ME_1A[, c(2, 10, 26, 9, 24, 3, 33, 25, 1, 12)]
-ME_2A_preserved <- ME_2A[, c(2, 10, 26, 9, 24, 3, 33, 25, 1, 12)]
+ME_1A_preserved <- ME_1A[, c(2, 10, 26, 9, 24, 3, 33, 25, 1, 12)]   # Subject to change
+ME_2A_preserved <- ME_2A[, c(2, 10, 26, 9, 24, 3, 33, 25, 1, 12)]   # Subject to change
