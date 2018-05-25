305c16225c4a2fdbb507665feb93470d3e67498a
Nitesh sabankar <niteshsabankar@gmail.com>
2018-05-25 05:08:10 -0700

    Update 12.relateToExtData.r

--- 12.relateToExtData.r
+++ 12.relateToExtData.r
@@ -6,7 +6,6 @@ biominerA1$module <- moduleColors
 biominerA1 <- biominerA1[, 9, drop=FALSE]
 biominerA1 <- data.frame(biominerA1, Biominer=biominer[match(row.names(biominerA1), biominer$ID), 2])
 biominerA1 <- na.omit(biominerA1)
-biominerA1[order(biominerA1$module),]
 write.table(biominerA1[order(biominerA1$module),], 
             file = "Biominer_modules.csv", 
             sep = ',', col.names = TRUE, 
@@ -27,7 +26,6 @@ lipidA1$module <- moduleColors
 lipidA1 <- lipidA1[, 9, drop=FALSE]
 lipidA1 <- data.frame(lipidA1, Lipid=lipid[match(row.names(lipidA1), lipid$ID), 2])
 lipidA1 <- na.omit(lipidA1)
-lipidA1[order(lipidA1$module),]
 write.table(lipidA1[order(lipidA1$module),], 
             file = "lipid_modules.csv", 
             sep = ',', col.names = TRUE, 
