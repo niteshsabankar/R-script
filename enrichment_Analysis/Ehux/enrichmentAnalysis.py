f34aada6b5497dc7f1dd3f8b9b2b23ed834fa407
Nitesh sabankar <niteshsabankar@gmail.com>
2018-07-10 15:36:52 -0700

    Update enrichmentAnalysis.py

--- enrichment_Analysis/Ehux/enrichmentAnalysis.py
+++ enrichment_Analysis/Ehux/enrichmentAnalysis.py
@@ -11,7 +11,7 @@ print(len(modules), "modules")
 print(modules)
 
 for module in modules:
-        command = "/w2/xyzhang/src/trinity/Analysis/DifferentialExpression/run_GOseq.pl --genes_single_factor " + module + " --GO_assignments geph_go.txt --lengths geph.seq_lens --background all.ids"
+        command = "/w2/xyzhang/src/trinity/Analysis/DifferentialExpression/run_GOseq.pl --genes_single_factor " + module + " --GO_assignments ehux_go.txt --lengths ehux.seq_lens --background all.ids"
         print command
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
         process.wait()
